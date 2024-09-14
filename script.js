d029f9f74a40c1a545d1f67df3d970692949d118
thanakorn55555 <thanakorn1450@gmail.com>
2024-09-14 11:56:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+const addEventOnElem = function (elem , type , callback) {
+    if(elem.length > 1) {
+        for(let i = 0 ; i < elem.length ; i++) {
+            elem[i].addEventListener(type,callback);
+        }
+    } else {
+        elem.addEventListener(type,callback);
+    }
+}
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNavbar = function () {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+}
+
+addEventOnElem(navTogglers, "click", toggleNavbar);
+
+const closeNavbar = function () {
+    navbar.classList.remove("active");
+    overlay.classList.remove("active");
+}
+
+addEventOnElem(navbarLinks, "click", closeNavbar);
+
+const header = document.querySelector("[data-header]");
+
+const headerActive = function () {
+    if(window.scrollY > 150) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+}
+
+addEventOnElem(window, "scroll", headerActive);
+
+let lastScrolledPos = 0;
+
+const headerSticky = function () {
+    if(lastScrolledPos >= window.scrollY) {
+        header.classList.remove("header-hide");
+    } else {
+        header.classList.add("header-hide");
+    }
+
+    lastScrolledPos = window.scrollY
+}
+
+addEventOnElem(window, "scroll", headerSticky);
+
+const section = document.querySelectorAll("[data-section]");
+
+const scrollReveal = function () {
+    for (let i = 0 ; i < section.length ; i++) {
+        if(section[i].getBoundingClientRect().top < window.innerHeight / 2) {
+            section[i].classList.add("active");
+        } else {
+            section[i].classList.remove("active");
+        }
+    }
+}
+
+scrollReveal();
+
+addEventOnElem(window, "scroll", scrollReveal);
